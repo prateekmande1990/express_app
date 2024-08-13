2af2d846a4ac83727069bde1ab70a0e48279347e
Pratik Mande <pratik.mande@springuplabs.com>
2024-08-13 11:52:01 +0530

    middleware function added

--- server.js
+++ server.js
@@ -6,30 +6,35 @@ const bodyParser = require("body-parser");
 require("dotenv").config();
 
 const app = express();
-app.use(cors());
-app.options("*", cors());
-app.use(compression());
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Method", "DELETE, PUT, POST, GET");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-app.use(bodyParser.json({ limit: "10mb" }));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(
-  cookieSession({
-    name: "express_app_session",
-    secret: process.env.SECRET_KEY,
-    httpOnly: true,
-    sameSite: "strict",
-  })
-);
+const setupMiddleware = (app) => {
+  app.use(cors());
+  app.options("*", cors());
+  app.use(compression());
 
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "DELETE, PUT, POST, GET");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
+
+  app.use(bodyParser.json({ limit: "10mb" }));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+
+  app.use(
+    cookieSession({
+      name: "express_app_session",
+      secret: process.env.SECRET_KEY,
+      httpOnly: true,
+      sameSite: "strict",
+    })
+  );
+};
+setupMiddleware(app);
 const db = require("./app/models");
 db.sequelize.sync();
 app.get("/", (req, res) => {
